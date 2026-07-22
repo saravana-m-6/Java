779c84252d8d9dbdaf0a9a0477029476a2c048de
saravana-m-6 <saravanamutharason@gmail.com>
2026-07-22 09:29:55 +0530

    Add files via upload

---
+++ day10/Vowels.java
@@ -0,0 +1,16 @@
+package day10;
+
+import java.util.Scanner;
+
+public class Vowels {
+    public static void main(String[]args){
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            char vowels = ('a','e','i','o','u');
+            String name = input.next();
+            char[] arr = name.toCharArray();
+
+
+
+    }
+}
